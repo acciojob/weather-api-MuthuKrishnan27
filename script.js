84282708e874077d3d69119bba7ada30040d7b75
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-24 14:18:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const weatherBtn = document.getElementById('weatherBtn');
+const weatherDataDiv = document.getElementById('weatherData');
+
+weatherBtn.addEventListener('click', getCurrentWeather);
+
+function getCurrentWeather() {
+  const apiKey = 'ea0e90b778511fe0241dcfc3796c8fdf'; // Replace with your OpenWeatherMap API key
+  const city = 'London';
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weather = data.weather[0].main;
+      const weatherData = `Current weather in ${city}: ${weather}`;
+      weatherDataDiv.textContent = weatherData;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      weatherDataDiv.textContent = 'Failed to fetch weather data';
+    });
+}
